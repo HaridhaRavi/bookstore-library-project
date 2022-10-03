e3030870cd1364a74cf9d96bd9ec75216301334e
HaridhaRavi <haridha.gr@gmail.com>
2022-10-03 12:27:56 +0200

    initial setup with Ironlauncher + add Book model + add seeds file

---
+++ models/Book.model.js
@@ -0,0 +1,16 @@
+// models/Book.model.js
+const { Schema, model } = require('mongoose');
+
+const bookSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    author: String,
+    rating: Number
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('Book', bookSchema);
