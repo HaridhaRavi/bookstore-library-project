aa8b46bc295117e48e9cbea5f3c8436f534b3b96
HaridhaRavi <haridha.gr@gmail.com>
2022-10-04 11:58:45 +0200

    add author model + update seed file +update views

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const Book = require("../models/Book.model");
+const Author = require("../models/Author.model");
 
 
 /* GET all the books from DB */
 //READ:get all books
 router.get("/books", (req, res, next) => {
     Book.find()
+        .populate("author")
         .then(booksFromDb =>{
             console.log(booksFromDb)
             res.render("books/books-list",{books: booksFromDb})    
@@ -19,6 +21,7 @@ router.get("/books", (req, res, next) => {
 router.get("/books/:bookId", (req, res, next) => {
     const bookId = req.params.bookId;
     Book.findById(bookId)
+        .populate("author")
         .then(bookDetails =>{
             //console.log(bookDetails)
             res.render("books/book-details",bookDetails)
